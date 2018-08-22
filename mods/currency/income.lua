cf3d3fbdbeee5770761f8718a90914e2952872d1
Elkien3 <Elkien3@users.noreply.github.com>
2018-08-21 20:30:46 -0500

    change amount given

--- mods/currency/income.lua
+++ mods/currency/income.lua
@@ -5,7 +5,7 @@ local function income()
     for _,player in ipairs(minetest.get_connected_players()) do
         local name = player:get_player_name()
 		local inv = player:get_inventory()
-        inv:add_item("main", "currency:minegeld_5")
+        inv:add_item("main", "currency:minegeld")
         minetest.log("info", "[Currency] basic income for "..name.."")
     end
 	minetest.after(600, income)
