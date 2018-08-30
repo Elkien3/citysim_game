8aeec36c3b6d3c6343a7c207841bb288212e5e54
Elkien3 <Elkien3@users.noreply.github.com>
2018-08-29 19:47:54 -0500

    tweak thingy

--- mods/technic-master/technic/machines/LV/cables.lua
+++ mods/technic-master/technic/machines/LV/cables.lua
@@ -2,7 +2,7 @@
 minetest.register_alias("lv_cable", "technic:lv_cable")
 
 minetest.register_craft({
-	output = 'technic:lv_cable 6',
+	output = 'technic:lv_cable 12',
 	recipe = {
 		{'default:paper',        'default:paper',        'default:paper'},
 		{'default:copper_ingot', 'default:copper_ingot', 'default:copper_ingot'},
