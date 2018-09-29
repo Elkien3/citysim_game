329150a962e8a34906a1835930dc374f109f5ec6
Elkien3 <Elkien3@users.noreply.github.com>
2018-09-28 21:39:00 -0500

    update digiboard to jogags version

--- mods/digiboard/init.lua
+++ mods/digiboard/init.lua
@@ -1,6 +1,9 @@
---Digiboard by bas080
+-- Original Digiboard mod by bas080
+-- Cracked by jogag
+-- Added features: settable channel, no more minetest.env, settable field caption (via digiline)
+
 minetest.register_node("digiboard:keyboard", {
-  description = "Digilines Keyboard",
+  description = "Digiboard",
   tiles = {"keyboard_top.png", "keyboard_bottom.png", "keyboard_side.png", "keyboard_side.png", "keyboard_side.png", "keyboard_side.png"},
   walkable = true,
   paramtype = "light",
@@ -21,22 +24,28 @@ minetest.register_node("digiboard:keyboard", {
   digiline = { receptor = {},
     effector = {
       action = function(pos, node, channel, msg)
+        local meta = minetest.get_meta(pos)
+        if channel == meta:get_string("channel") then
+          meta:set_string("formspec", "field[text;"..msg..";]")
+        end
       end
     },
   },
   groups =  {choppy = 3, dig_immediate = 2},
   on_construct = function(pos)
-    local meta = minetest.env:get_meta(pos)
-    meta:set_string("formspec", "field[text;;]")
+    local meta = minetest.get_meta(pos)
+    meta:set_string("formspec", "field[channel;Channel;]")
     meta:set_string("infotext", "Keyboard")
     meta:set_int("lines", 0)
   end,
   on_receive_fields = function(pos, formname, fields, sender)
-    local meta = minetest.env:get_meta(pos)
-    local text = fields.text
-    local channel = "keyboard"
-    if text ~= nil then
-      digiline:receptor_send(pos, digiline.rules.default, channel, text)
+    local meta = minetest.get_meta(pos)
+    local channel = meta:get_string("channel")
+    if fields.channel then
+      meta:set_string("channel", fields.channel)
+      meta:set_string("formspec", "field[text;Enter text;]")
+    elseif fields.text then
+      digiline:receptor_send(pos, digiline.rules.default, channel, fields.text)
     end
   end,
 })
