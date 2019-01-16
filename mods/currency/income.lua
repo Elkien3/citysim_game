2ba1842dbb521db3b9d5fdb7f2340e778a35c91f
Elkien3 <Elkien3@users.noreply.github.com>
2019-01-16 16:54:31 -0600

    make currency income support mumblereward

--- mods/currency/income.lua
+++ mods/currency/income.lua
@@ -4,6 +4,9 @@ local timer = 0
 local function income()
     for _,player in ipairs(minetest.get_connected_players()) do
         local name = player:get_player_name()
+		if minetest.get_modpath("mumblereward") ~= nil then
+			if not mumblereward_players[name] then return end
+		end
 		local inv = player:get_inventory()
         inv:add_item("main", "currency:minegeld")
         minetest.log("info", "[Currency] basic income for "..name.."")
