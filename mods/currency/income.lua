95737e1a53e616216fca92fe56340ba06a646bca
Elkien3 <Elkien3@users.noreply.github.com>
2019-01-17 12:26:04 -0600

    fix income only giving if all players are on mumble

--- mods/currency/income.lua
+++ mods/currency/income.lua
@@ -4,12 +4,17 @@ local timer = 0
 local function income()
     for _,player in ipairs(minetest.get_connected_players()) do
         local name = player:get_player_name()
+		local givetoplayer = true
 		if minetest.get_modpath("mumblereward") ~= nil then
-			if not mumblereward_players[name] then return end
+			if not mumblereward_players[name] then
+				givetoplayer = false 
+			end
 		end
-		local inv = player:get_inventory()
-        inv:add_item("main", "currency:minegeld")
-        minetest.log("info", "[Currency] basic income for "..name.."")
+		if givetoplayer then
+			local inv = player:get_inventory()
+			inv:add_item("main", "currency:minegeld")
+			minetest.log("info", "[Currency] basic income for "..name.."")
+		end
     end
 	minetest.after(600, income)
 end
