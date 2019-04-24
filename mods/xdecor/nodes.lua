9f06ed5aeac6be15ee7851c56d7e1924c4e9216b
Elkien3 <Elkien3@users.noreply.github.com>
2019-04-24 15:54:48 -0500

    fix craft

--- mods/xdecor/nodes.lua
+++ mods/xdecor/nodes.lua
@@ -301,13 +301,13 @@ local xdecor_doors = {
 		{"default:paper", "group:wood"},
 		{"group:wood", "default:paper"} },
 	prison = {
-		{"xpanes:bar", "xpanes:bar"},
-		{"xpanes:bar", "xpanes:bar"},
-		{"xpanes:bar", "xpanes:bar"} },
+		{"xpanes:bar_flat", "xpanes:bar_flat"},
+		{"xpanes:bar_flat", "xpanes:bar_flat"},
+		{"xpanes:bar_flat", "xpanes:bar_flat"} },
 	rusty_prison = {
-		{"xpanes:rusty_bar", "xpanes:rusty_bar"},
-		{"xpanes:rusty_bar", "xpanes:rusty_bar"},
-		{"xpanes:rusty_bar", "xpanes:rusty_bar"} },
+		{"xpanes:rusty_bar_flat", "xpanes:rusty_bar_flat"},
+		{"xpanes:rusty_bar_flat", "xpanes:rusty_bar_flat"},
+		{"xpanes:rusty_bar_flat", "xpanes:rusty_bar_flat"} },
 	--screen = {
 	--	{"group:wood", "group:wood"},
 	--	{"xpanes:chainlink", "xpanes:chainlink"},
