470e8d2b7d15cc74e166f270b9b903af5f47976d
Elkien3 <Elkien3@users.noreply.github.com>
2019-10-02 11:49:48 -0500

    add NIC

---
+++ mods/digistuff/init.lua
@@ -0,0 +1,9 @@
+digistuff = {}
+
+local http = minetest.request_http_api()
+if not http then
+	minetest.log("error","digistuff is not allowed to use the HTTP API - digilines NIC will not be available!")
+	minetest.log("error","If this functionality is desired, please add digistuff to your secure.http_mods setting")
+else
+	loadfile(string.format("%s%s%s.lua",minetest.get_modpath(minetest.get_current_modname()),DIR_DELIM,"nic"))(http)
+end
