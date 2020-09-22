4b71bf1e1e0e78d521bfb352b7d812f9bf0632ed
Lars Mueller <appgurulars@gmx.de>
2020-09-22 17:17:34 +0200

    Remove character_anim

--- mods/character_anim/init.lua
+++
@@ -1,8 +0,0 @@
-setfenv(1, modlib.mod)
-local namespace = create_namespace()
-local quaternion = setmetatable({}, {__index = namespace})
-include_env(get_resource"quaternion.lua", quaternion)
-namespace.quaternion = quaternion
-extend"conf"
-extend"importer"
-extend"main"
