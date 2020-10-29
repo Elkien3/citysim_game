e0b50320f66c86f1e4343e4047c2d4c76ed32453
Elkien3 <Elkien3@users.noreply.github.com>
2020-10-29 11:54:04 -0500

    add mumble only period capability

--- mods/mumblereward/init.lua
+++ mods/mumblereward/init.lua
@@ -4,6 +4,8 @@ local port = minetest.settings:get("port") or "30000"
 mumblereward_players = {}
 local formtimer = {}
 
+local mumbleonly = minetest.settings:get_bool("mumbleonly") or false
+
 local mutetags = {}
 minetest.register_entity("mumblereward:tag", {
 	physical = false,
@@ -55,28 +57,23 @@ local function checkfile()
 	for line in input:lines() do
 			local data = string.split(line, " ")
 			local name = data[1]
-			local game = data [2]
-			local ipport = data[3]
-			local chan = data[4]
-			local deaf = data[5] == "True"
+			local context = data [2]
+			local chan = data[3]
+			local deaf = data[4] == "True"
 			if minetest.get_player_by_name(name) then
-				if game ~= "Minetest" then
+				if context == "quit" then
 					mumblereward_players[name] = nil
-					if game == "quit" then
-						minetest.chat_send_player(name, "*!Mumblerewards!* Disconnected from Positional Audio. Reason: You quit Mumble.")
-					else
-						minetest.chat_send_player(name, "*!Mumblerewards!* Disconnected from Positional Audio. Reason: You are not in Minetest.")
-					end
-				elseif ipport ~= ip..":"..port then
+					minetest.chat_send_player(name, "*!Mumblerewards!* Disconnected from Positional Audio. Reason: You quit Mumble.")
+				elseif context ~= "TWluZXRlc3QAMTM4LjE5Ny4yMi4xM" then
 					mumblereward_players[name] = nil
-					minetest.chat_send_player(name, "*!Mumblerewards!* Disconnected from Positional Audio. Reason: incorrect context: '"..ipport.."' Double check that you're using minetest-mumble-wrapper, the CSM is enabled, and Mumble PA is enabled.")
+					minetest.chat_send_player(name, "*!Mumblerewards!* Disconnected from Positional Audio. Reason: Incorrect Server/Context.")
 				elseif chan ~= channel then
 					mumblereward_players[name] = nil
 					minetest.chat_send_player(name, "*!Mumblerewards!* Disconnected from Positional Audio. Reason: Not in the correct mumble channel.")
 				elseif deaf then
 					mumblereward_players[name] = nil
 					minetest.chat_send_player(name, "*!Mumblerewards!* Disconnected from Positional Audio. Reason: You have deafened yourself.")
-				else
+				elseif mumblereward_players[name] ~= true then
 					mumblereward_players[name] = true
 					minetest.chat_send_player(name, "*!Mumblerewards!* Connected with Positional Audio!")
 					removetag(name)
@@ -89,9 +86,9 @@ local function checkfile()
 	output:write("")
 	io.close(output)
 	dotag()
-	minetest.after(5, checkfile)
+	minetest.after(1, checkfile)
 end
-minetest.after(5, checkfile)
+minetest.after(1, checkfile)
 
 local newline = "                                                                                                     "
 local function mumbleform(name)
@@ -99,7 +96,7 @@ local function mumbleform(name)
     local form = "" ..
     "size[8,8]" ..
     "image[0.7,0.5;8.2,4.8333333333333;mumbleimage.png]" ..
-    "textarea[1,5;5,3;TextArea;;Set up Mumble Positional Audio with Minetest and be able to hear other players (you don't necessarily have to talk), have no mute symbol, and get FREE minegelds!"..newline.."|"..newline.."Demo vid by Minetest Videos: https://youtu.be/6AsHD9h8IE8 |"..newline.."Tutorial: https://youtu.be/lP7zEydOIEI |"..newline.."Github: https://github.com/chipgw/minetest-mumble-wrapper |       Mumble: https://www.mumble.com/mumble-download.php |"..newline.."|"..newline.."Type in /mumble to view this page again, and /mumble playername to see if a certain player is connected.]" ..
+    "textarea[1,5;5,3;TextArea;;Set up Mumble Positional Audio with Minetest and be able to hear other players (you don't necessarily have to talk), have no mute symbol, and other possible future perks!"..newline.."|"..newline.."Demo vid by Minetest Videos: https://youtu.be/6AsHD9h8IE8 |"..newline.."Tutorial: https://youtu.be/lP7zEydOIEI |"..newline.."Github: https://github.com/Elkien3/minetest-mumble-wrapper |       Mumble: https://www.mumble.com/mumble-download.php |         Discord: https://discord.gg/nG8ZgF9 |"..newline.."|"..newline.."Type in /mumble to view this page again, and /mumble playername to see if a certain player is connected.]" ..
     "button_exit[6,7;1.5,0.5;Close;Close \\[   \\]]" ..
     "label[6.83,7;"..minetest.formspec_escape(tostring(formtimer[name])).."]" ..
     ""
@@ -108,18 +105,27 @@ local function mumbleform(name)
 end
 
 local function countdown(name)
-	if formtimer[name] == nil or formtimer[name] == 0 then return end
+	if mumblereward_players[name] then formtimer[name] = 0 end
 	formtimer[name] = formtimer[name] - 1
 	minetest.show_formspec(name, "mumbleformspec", mumbleform(name))
 	minetest.after(1, countdown, name)
 end
 
-local function checkplayer(name)
+local function checkplayer(name, kick)
 	if not name then return end
 	if not mumblereward_players[name] then
-		--minetest.chat_send_player(name, "*!Mumblerewards!* Not connected to Positional Audio.")
-		formtimer[name] = 3
-		minetest.after(1, countdown, name)
+		minetest.chat_send_player(name, "*!Mumblerewards!* Not connected to Positional Audio.")
+		if mumbleonly then
+			local val, timetil, length = is_mumbleonly()
+			if kick then
+				minetest.kick_player(name, "Server is in a mumble-only period, set up minetest with mumble PA or come back in ".. -timetil .." minutes.")
+			elseif val then
+				minetest.chat_send_player(name, "*!Mumblerewards!* The server is in a mumble-only period for "..-timetil.." more minutes, you will be kicked in 30 seconds.")
+				minetest.after(30, checkplayer, name, true)
+			end
+		end
+		--formtimer[name] = 0
+		--minetest.after(1, countdown, name)
 		minetest.show_formspec(name, "mumbleformspec", mumbleform(name))
 	end
 end
@@ -128,8 +134,8 @@ minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
 	addtag(name)
 	local privs = minetest.get_player_privs(name)
-	if privs.interact and not privs.ban then
-		minetest.after(25, checkplayer, name)
+	if (privs.interact or (mumbleonly and is_mumbleonly())) and not privs.ban then
+		minetest.after(30, checkplayer, name)
 	end
 end)
 
@@ -163,3 +169,8 @@ minetest.register_chatcommand("mumble", {func = function(name, param)
 		return true, "Opened Mumble Info!"
 	end
 end})
+
+if mumbleonly then
+	local path = minetest.get_modpath(minetest.get_current_modname())
+	dofile(path .. "/mumbleonly.lua")
+end
---
+++ mods/mumblereward/mumbleonly.lua
@@ -0,0 +1,98 @@
+local function minute_timeofday()
+	local date = os.date("*t")
+	local minutes = date.min + (date.hour*60)
+	return minutes, date.sec
+end
+
+local function send_chat_all(message)
+	minetest.chat_send_all(message)
+	if irc then
+		irc.send(message)
+	end
+end
+
+local function readable_minutes(minutes)
+	local hours = 0
+	while minutes >= 60 do
+		minutes = minutes - 60
+		hours = hours + 1
+	end
+	if hours > 0 then
+		return hours.." hours, "..minutes.." minutes"
+	else
+		return minutes.." minutes"
+	end
+end
+
+local mumbleonly_periods = {{14*60, 30}, {2*60, 30}}
+
+function is_mumbleonly()
+	local timetil
+	local minutes = minute_timeofday()
+	local length
+	for k, period in pairs(mumbleonly_periods) do
+		local til = period[1]-minutes
+		local sign = 0
+		if til ~= 0 then sign = til/math.abs(til) end
+		if til == 0 or (sign == -1 and til + period[2] > 0) then -- youre within the period
+			return true, -1*(til + period[2]), period[2] -- return time remaining as negative
+		end
+		if (not timetil or timetil > til) and sign ~= -1 then
+			timetil = til
+			length = period[2]
+		end
+	end
+	if not timetil then -- none left today, look at earliest one tomorrow
+		for k, period in pairs(mumbleonly_periods) do
+			local til = period[1]
+			if not timetil or timetil > til then
+				timetil = til
+				length = period[2]
+			end
+		end
+		timetil = timetil + (24*60-minutes)
+	end
+	return false, timetil, length
+end
+
+local function kick_nonmumble(length)
+	for _, player in pairs (minetest.get_connected_players()) do
+		local name = player:get_player_name()
+		if not mumblereward_players[name] and not minetest.get_player_privs(name).ban then
+			minetest.kick_player(name, "Server is in a mumble-only period, set up minetest with mumble PA or come back in ".. length .." minutes.")
+		end
+	end
+end
+
+local function do_chats()
+	local val, timetil, length = is_mumbleonly()
+	local minutes, sec = minute_timeofday()
+	if val then --currently in a period
+		send_chat_all("*!Mumblerewards!* Mumble only period going on, ending in "..readable_minutes(math.abs(timetil)))
+		minetest.after((math.abs(timetil)*60)-sec, function() do_chats() local til, tim = is_mumbleonly() send_chat_all("*!Mumblerewards!* Mumble only period over, next one in "..readable_minutes(tim)) end)
+	else
+		minetest.after((math.abs(timetil)*60)-sec, function() kick_nonmumble(length) send_chat_all("*!Mumblerewards!* Mumble only period starting, ending in "..readable_minutes(length)) end)
+		if timetil >= 10 then
+			minetest.after((math.abs(timetil-1)*60)-sec, function() send_chat_all("*!Mumblerewards!* Mumble only period starting in 1 minute.") end)
+		end
+		if timetil >= 5 then
+			minetest.after((math.abs(timetil-5)*60)-sec, function() send_chat_all("*!Mumblerewards!* Mumble only period starting in 5 minute.") end)
+		end
+		if timetil >= 1 then
+			minetest.after((math.abs(timetil-1)*60)-sec, function() send_chat_all("*!Mumblerewards!* Mumble only period starting in 1 minute.") end)
+			minetest.after(((math.abs(timetil-1)*60)-sec)+50, function() send_chat_all("*!Mumblerewards!* Mumble only period starting in 10 seconds.") end)
+		end
+		minetest.after((math.abs(timetil+length)*60)-sec, function() do_chats() local til, tim = is_mumbleonly() send_chat_all("*!Mumblerewards!* Mumble only period over, next one in "..readable_minutes(tim)) end)
+	end
+end
+
+minetest.after(10, do_chats)
+
+minetest.register_chatcommand("mumbleonly", {func = function(name, param)
+	local val, timetil, length = is_mumbleonly()
+	if val then
+		return true, "*!Mumblerewards!* Currently in mumble only period, over in "..readable_minutes(-timetil)
+	else
+		return true, "*!Mumblerewards!* Next mumble only period is in "..readable_minutes(timetil)
+	end
+end})
