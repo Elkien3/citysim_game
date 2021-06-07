4494b3d97fc17ea7b627c2f1100ee9ede344959c
Elkien3 <Elkien3@users.noreply.github.com>
2021-06-07 12:19:31 -0500

    tweak solar panel
    i made them have a sinus output during the day, starting from 0 at .25 day time and 200 at .5. also made them slowly decay which then requires more silicon wafers to fix.

--- mods/technic/technic/machines/LV/solar_panel.lua
+++ mods/technic/technic/machines/LV/solar_panel.lua
@@ -27,17 +27,22 @@ local run = function(pos, node)
 
 	local light = minetest.get_node_light(pos1, nil)
 	local time_of_day = minetest.get_timeofday()
+	light = minetest.get_natural_light(pos)
 	local meta = minetest.get_meta(pos)
 	if light == nil then light = 0 end
 	-- turn on panel only during day time and if sufficient light
         -- I know this is counter intuitive when cheating by using other light sources underground.
-	if light >= 12 and time_of_day >= 0.24 and time_of_day <= 0.76 and pos.y > -10 then
-		local charge_to_give = math.floor((light + pos1.y) * 3)
+	if light >= 14 and time_of_day >= 0.24 and time_of_day <= 0.76 and pos.y > -10 then
+		local charge_to_give = math.floor(math.sin(math.pi*2*(time_of_day+.75)) * 200)+1
 		charge_to_give = math.max(charge_to_give, 0)
 		charge_to_give = math.min(charge_to_give, 200)
 		meta:set_string("infotext", S("@1 Active (@2)", machine_name,
 			technic.EU_string(charge_to_give)))
 		meta:set_int("LV_EU_supply", charge_to_give)
+		if math.random(7560000) == 1 then --average of one per 25 panel each week needing repair (if my math/logic is right)
+			node.name = "technic:solar_panel_damaged"
+			minetest.set_node(pos, node)
+		end
 	else
 		meta:set_string("infotext", S("%s Idle"):format(machine_name))
 		meta:set_int("LV_EU_supply", 0)
@@ -70,3 +75,30 @@ minetest.register_node("technic:solar_panel", {
 
 technic.register_machine("LV", "technic:solar_panel", technic.producer)
 
+minetest.register_node("technic:solar_panel_damaged", {
+	tiles = {"technic_solar_panel_top.png^[colorize:#000000:100",  "technic_solar_panel_bottom.png", "technic_solar_panel_side.png",
+	         "technic_solar_panel_side.png", "technic_solar_panel_side.png",   "technic_solar_panel_side.png"},
+	groups = {snappy=2, choppy=2, oddly_breakable_by_hand=2,
+		technic_machine=1, technic_lv=1},
+	connect_sides = {"bottom"},
+	sounds = default.node_sound_wood_defaults(),
+	description = S("Small Solar %s Generator (Damaged)"):format("LV"),
+	active = false,
+	drawtype = "nodebox",
+	paramtype = "light",
+	is_ground_content = true,
+	node_box = {
+		type = "fixed",
+		fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+	},
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_int("LV_EU_supply", 0)
+		meta:set_string("infotext", S("Small Solar %s Generator (Damaged)"):format("LV"))
+	end,
+})
+minetest.register_craft({
+	output = 'technic:solar_panel',
+	type = "shapeless",
+	recipe =  {'technic:doped_silicon_wafer', 'technic:solar_panel_damaged'},
+})
