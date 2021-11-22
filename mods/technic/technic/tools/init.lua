f5bc8631b46fcc9a212e3060707b654f1f25c689
Elkien3 <Elkien3@users.noreply.github.com>
2021-11-21 20:11:55 -0600

    remove lasers, drills, and chainsaws

--- mods/technic/technic/tools/init.lua
+++ mods/technic/technic/tools/init.lua
@@ -1,16 +1,16 @@
 local path = technic.modpath.."/tools"
-
+--[[
 if technic.config:get_bool("enable_mining_drill") then
 	dofile(path.."/mining_drill.lua")
 end
 if technic.config:get_bool("enable_mining_laser") then
 	dofile(path.."/mining_lasers.lua")
-end
+end--]]
 if technic.config:get_bool("enable_flashlight") then
 	dofile(path.."/flashlight.lua")
 end
 dofile(path.."/cans.lua")
-dofile(path.."/chainsaw.lua")
+--dofile(path.."/chainsaw.lua")
 dofile(path.."/tree_tap.lua")
 dofile(path.."/sonic_screwdriver.lua")
 dofile(path.."/prospector.lua")
