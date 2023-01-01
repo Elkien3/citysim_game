8c03c1869cbfd6c86244fd6148903452f86dca07
Elkien3 <Elkien3@users.noreply.github.com>
2023-01-01 17:52:11 -0600

    Update init.lua

--- mods/default_tweaks/init.lua
+++ mods/default_tweaks/init.lua
@@ -184,7 +184,7 @@ minetest.register_on_mods_loaded(function()
 end)
 
 if minetest.registered_items["xpanes:door_steel_bar"] and minetest.registered_items["doors:prison_door"] then
-	minetest.clear_craft({output = "xpanes:door_steel_bar"})
+	--minetest.clear_craft({output = "xpanes:door_steel_bar"})
 	minetest.register_craft({
 		output = "xpanes:door_steel_bar",
 		type = "shapeless",
