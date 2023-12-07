d7a7b42bfbb8ff3f8c3cdd4fd805abbd0a7a2a7c
Elkien3 <Elkien3@users.noreply.github.com>
2023-12-07 11:23:29 -0600

    Update init.lua

--- mods/antilag/init.lua
+++ mods/antilag/init.lua
@@ -15,7 +15,9 @@ minetest.register_globalstep(function(lag)
 end)
 
 minetest.register_on_leaveplayer(function(player, timed_out)
-	if minetest.get_server_uptime() > 21600 and #minetest.get_connected_players() == 0 then
-		minetest.request_shutdown("Server rebooting to prevent lag.")
-	end
+	minetest.after(2, function()
+		if minetest.get_server_uptime() > 21600 and #minetest.get_connected_players() == 0 then
+			minetest.request_shutdown("Server rebooting to prevent lag.")
+		end
+	end)
 end)
