5f870513951e3ee74d4b4a3e040387fd85a0aafd
Moji ( <84784562+xMoji@users.noreply.github.com>
2023-12-13 16:00:41 +0100

    Update init.lua
    Fix elevator again after first fix not working

--- mods/elevator/init.lua
+++ mods/elevator/init.lua
@@ -460,7 +460,7 @@ for _,mode in ipairs({"on", "off"}) do
         on_place = function(itemstack, placer, pointed_thing)
             local pos  = pointed_thing.above
             local node = minetest.get_node(vector.add(pos, {x=0, y=1, z=0}))
-            if (node ~= nil and node.drawtype ~= "airlike" and node.name ~= placeholder) then
+            if node == nil or (minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].drawtype ~= "airlike" and node.name ~= placeholder) then
                 return
             end
             return minetest.item_place(itemstack, placer, pointed_thing)
