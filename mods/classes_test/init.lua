9fdcb806565e704ce9ad2c15ebea259c0bcb4cb3
Elkien3 <Elkien3@users.noreply.github.com>
2024-09-15 16:46:11 -0500

    remove lockpicks

--- mods/classes_test/init.lua
+++ mods/classes_test/init.lua
@@ -182,3 +182,55 @@ if minetest.get_modpath("technic") then
 		end
 	end
 end
+
+if minetest.get_modpath("default") then
+	--allow snoopers to rightclick chests to peek into them.
+	--self-explanatory - taken from original locked chest code
+	function has_locked_chest_privilege(meta, player)
+		if player:get_player_name() ~= meta:get_string("owner") then
+			return false
+		end
+		return true
+	end
+
+	function get_chest_formspec(pos)
+		local spos = pos.x .. "," .. pos.y .. "," .. pos.z
+		local formspec =
+			"size[8,9]" ..
+			default.gui_bg ..
+			default.gui_bg_img ..
+			default.gui_slots ..
+			"list[nodemeta:" .. spos .. ";main;0,0.3;8,4;]" ..
+			"list[current_player;main;0,4.85;8,1;]" ..
+			"list[current_player;main;0,6.08;8,3;8]" ..
+			"listring[nodemeta:" .. spos .. ";main]" ..
+			"listring[current_player;main]" ..
+			default.get_hotbar_bg(0,4.85)
+		return formspec
+	end
+			
+	local newchestrightclick = function(pos, node, clicker, itemstack, pointed_thing)
+		local wield = clicker:get_wielded_item():get_name()
+		local def = minetest.registered_tools[wield]
+		if not default.can_interact_with_node(clicker, pos) and not class_get(clicker:get_player_name(), "snooper") then
+			return itemstack
+		end
+		name = "default:chest_locked"
+		sound_open = "default_chest_open"
+		sound_close = "default_chest_close"
+		minetest.sound_play(sound_open, {gain = 0.3,
+				pos = pos, max_hear_distance = 10}, true)
+		if not default.chest.chest_lid_obstructed(pos) then
+			minetest.swap_node(pos,
+					{ name = name .. "_open",
+					param2 = node.param2 })
+		end
+		minetest.after(0.2, minetest.show_formspec,
+				clicker:get_player_name(),
+				"default:chest", default.chest.get_chest_formspec(pos))
+		default.chest.open_chests[clicker:get_player_name()] = { pos = pos,
+				sound = sound_close, swap = name }
+	end
+	minetest.override_item("default:chest_locked", {on_rightclick = newchestrightclick})
+	minetest.override_item("default:chest_locked_open", {on_rightclick = newchestrightclick})
+end
--- mods/lockpicks/README.md
+++
@@ -1,6 +0,0 @@
-lockpicks
-=========
-
-Mod to add lockpicks to MineTest
-
-See forum info at: https://forum.minetest.net/viewtopic.php?f=9&t=9224
