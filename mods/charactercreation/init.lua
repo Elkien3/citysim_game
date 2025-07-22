973d8663607b9effb8aed92433a936936827e18c
Elkien3 <Elkien3@users.noreply.github.com>
2025-07-22 13:31:34 -0500

    Update init.lua

--- mods/charactercreation/init.lua
+++ mods/charactercreation/init.lua
@@ -40,7 +40,7 @@ local function doskinny(player, skindata)
 	if not sd then sd = minetest.deserialize(mod_storage:get_string(name)) end
 	if not sd or sd == "" then sd = defaultskin end
 	for attribute, val in pairs(defaultskin) do
-		if not sd[attribute] then sd[attribute] = val
+		if not sd[attribute] then sd[attribute] = val end
 	end
 	local skin
 	if spood_get_effect then
